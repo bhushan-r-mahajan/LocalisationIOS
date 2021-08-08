b1c4f318f9f5eeeb955d3922c53648af143a4234
Bhushan Mahajan <bhushanmahajan1997@gmail.com>
2021-08-08 15:24:56 +0530

    Localisation for IOS apps demo

--- Localisation/ViewController.swift
+++ Localisation/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let languageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello".localize()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(languageLabel)
+        languageLabel.frame = view.bounds
     }
+}
 
-
+extension String {
+    
+    func localize() -> String {
+        return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
+    }
 }
 
